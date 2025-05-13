c9c26685da03e775886643e9acc11d3ad40e52e3
lvsi <15239928381@163.com>
2025-05-13 19:20:57 +0800

    feat: make_conversation_history函数新增only_user参数

--- wpylib/pkg/langchain/history.py
+++ wpylib/pkg/langchain/history.py
@@ -6,15 +6,19 @@
 from wpylib.util.x.xtyping import is_none
 
 
-def make_conversation_history(messages: list = None) -> str:
+def make_conversation_history(messages: list = None, only_user: bool = False) -> str:
     """
     封装上下文
     :param messages: 消息记录
+    :param only_user: 是否只拼装user角色的消息
     :return:
     """
     conversation_history = ""
     if is_none(messages):
         messages = []
     for message in messages:
-        conversation_history += f"\nUser: {message['query']}\nAssistant: {message['answer']}\n"
+        if only_user:
+            conversation_history += f"\nUser: {message['query']}\n"
+        else:
+            conversation_history += f"\nUser: {message['query']}\nAssistant: {message['answer']}\n"
     return f"<ConversationHistory>{conversation_history}</ConversationHistory>"
